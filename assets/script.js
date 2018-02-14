b9f292f0d05c4b40d13650304a8a0d4af8af2e4a
Corey Richard <ullrtheglorious@gmail.com>
2018-02-13 21:57:21 -0700

    changing some values

--- assets/script.js
+++ assets/script.js
@@ -5,23 +5,31 @@ var rpg = {
     yennefer: $('.yennefer_player'),
     triss: $('.triss_player'),
     starting_bg: $(".starting_bg"),
+ 
+    
 
     charSelect: function() {
     
     this.ciri.click(function (){
         rpg.starting_bg.toggleClass('ciri_bg');
+        $(".startingTitle").text("");
+        
     });
 
     this.geralt.click(function () {
         rpg.starting_bg.toggleClass('geralt_bg');
+        $(".startingTitle").text("");
+        
     });
 
     this.yennefer.click(function () {
         rpg.starting_bg.toggleClass('yennefer_bg');
+        $(".startingTitle").text("");
     });
     
     this.triss.click(function () {
         rpg.starting_bg.toggleClass('triss_bg');
+        $(".startingTitle").text("");
     });
     
     }
